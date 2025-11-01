510ce772a954a0c719ae9f3fffb67b474f689582
Zynest <96866421+Zynest@users.noreply.github.com>
2025-11-01 01:56:28 -0300

    le puse sonido de explosión (no funciona xd)

--- Scripts/explosion.gd
+++ Scripts/explosion.gd
@@ -1,6 +1,8 @@
 extends AnimatedSprite2D
+@onready var sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready():
 	play()
+	sound.play()
 	await get_tree().create_timer(0.8).timeout
 	queue_free()
