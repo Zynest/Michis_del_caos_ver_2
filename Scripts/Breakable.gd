f1a5bcb42405b3e342bf61a83c1fed4cda5b09fb
Zynest <96866421+Zynest@users.noreply.github.com>
2025-11-01 12:41:02 -0300

    Move explosion sound to Breakable and clean up scripts
    Explosion sound playback is now handled in Breakable.gd instead of explosion.gd, improving separation of concerns. Unused code and comments were removed from Game.gd and room_1.gd for clarity.

--- Scripts/Breakable.gd
+++ Scripts/Breakable.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 @export var score_value = 100
 #Importo escena de explosión.
 @export var explosion_scene: PackedScene = preload("res://Rooms/explosion.tscn")
+@onready var sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Ground"):
@@ -14,5 +15,6 @@ func _break():
 		var explosion = explosion_scene.instantiate()
 		explosion.global_position = global_position
 		get_tree().current_scene.add_child(explosion)
+		sound.play()
 
 	call_deferred("queue_free")
--- Scripts/explosion.gd
+++ Scripts/explosion.gd
@@ -1,8 +1,8 @@
 extends AnimatedSprite2D
-@onready var sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
+#@onready var sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready():
 	play()
-	sound.play()
+	#sound.play()
 	await get_tree().create_timer(0.8).timeout
 	queue_free()
