949c27ced167167001bbab63f500f4d74e79c7c2
Zynest <96866421+Zynest@users.noreply.github.com>
2025-10-31 22:40:23 -0300

    Pausa agregada

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -9,25 +9,34 @@ func reset():
 	score = 0
 	time_left = 20.0
 	is_game_over = false
+	get_tree().paused = false
 
 func add_score(v:int) -> void:
 	score += v
 	if score >= target_score and not is_game_over:
+		is_game_over = true
 		await get_tree().process_frame
 		get_tree().change_scene_to_file("res://Rooms/pantalla_de_ganar.tscn")
+		await get_tree().process_frame
+		get_tree().paused = true
 
 func _process(delta):
 	if is_game_over:
-		return #no se ejecuta lo de abajo si es true
+		return # no se ejecuta lo de abajo si es true
 	time_left -= delta
 	if time_left <= 0:
 		game_over()
-		
+
 func game_over():
 	is_game_over = true
+	await get_tree().process_frame
 	get_tree().change_scene_to_file("res://Rooms/pantalla_de_perder.tscn")
+	await get_tree().process_frame
+	get_tree().paused = true
 
 func restart_game() -> void:
 	reset()
 	get_tree().reload_current_scene()
+	get_tree().paused = false
+
 		
--- Scripts/pantalla_de_perder.gd
+++ Scripts/pantalla_de_perder.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+
 func _on_salir_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Rooms/Room_1.tscn")
