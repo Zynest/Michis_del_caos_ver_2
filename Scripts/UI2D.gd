d7bb7a29d91918d5b5afd2a1ad4055d003d475ea
Zynest <96866421+Zynest@users.noreply.github.com>
2025-11-09 21:06:15 -0300

    Add new assets and update win/lose/menu screens
    Added new image assets (copa.png, howtoplay.png, zoomies_attack.png) and updated their usage in Room_1 and win/lose/menu scenes. Improved layout and collision shapes in Room_1, added a TileMapLayer to the win screen, and made minor UI adjustments for better appearance and interaction.

--- Scripts/UI2D.gd
+++ Scripts/UI2D.gd
@@ -2,4 +2,4 @@ extends CanvasLayer
 @onready var hud: Label = $HudLabel
 
 func _process(_delta):
-	hud.text = "Puntos: %d      Tiempo: %d" % [Game.score, int(Game.time_left)]
+	hud.text = "Score: %d      Time: %d" % [Game.score, int(Game.time_left)]
